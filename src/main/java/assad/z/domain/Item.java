908367a0a780ac716a034458f887ddd49c7f04c8
Assad <Assad.Zahieer@acedmytrainee.com>
2019-04-17 16:30:51 +0100

    Created tables(user, item) using JPA

---
+++ src/main/java/assad/z/domain/Item.java
@@ -0,0 +1,71 @@
+package assad.z.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+    private Long user_id;
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Id
+    private Long item_id;
+    private String item;
+    private Date date;
+    private boolean status = true;
+
+    public Item() {
+
+    }
+
+    public Item(String item) {
+        this.item = item;
+
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public Long getItem_id() {
+        return item_id;
+    }
+
+    public void setItem_id(Long item_id) {
+        this.item_id = item_id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+}
+
