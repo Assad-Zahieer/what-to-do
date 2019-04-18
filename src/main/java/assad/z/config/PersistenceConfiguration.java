a558f233079d4f1f8167573bb91275265576b93b
Assad <aa.zahieer@gmail.com>
2019-04-18 10:56:28 +0100

    Items API developed

---
+++ src/main/java/assad/z/config/PersistenceConfiguration.java
@@ -0,0 +1,22 @@
+package assad.z.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+
+@PropertySource("application.properties")
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Primary
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+}
