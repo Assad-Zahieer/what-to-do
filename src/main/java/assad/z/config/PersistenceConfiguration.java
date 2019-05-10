fd191a6693b8d00b9c1ffec2453c331fe5c86b33
Assad Zahieer <aa.zahieer@gmail.com>
2019-05-10 10:22:40 +0100

    modified persistence config

--- src/main/java/assad/z/config/PersistenceConfiguration.java
+++ src/main/java/assad/z/config/PersistenceConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 import javax.sql.DataSource;
 
@@ -22,10 +22,6 @@ public class PersistenceConfiguration {
     }
 
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder;
-    }
+
 
 }
